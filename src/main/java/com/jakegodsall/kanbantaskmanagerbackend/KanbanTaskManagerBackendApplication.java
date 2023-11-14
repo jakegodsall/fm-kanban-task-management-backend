685bde30e2fb1ca392cddb6706f01db3e6d136e9
jakegodsall <jake.edward.godsall@gmail.com>
2023-11-14 08:22:19 +0000

    initial commit

---
+++ src/main/java/com/jakegodsall/kanbantaskmanagerbackend/KanbanTaskManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.kanbantaskmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanTaskManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanTaskManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jakegodsall/kanbantaskmanagerbackend/KanbanTaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.kanbantaskmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanTaskManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

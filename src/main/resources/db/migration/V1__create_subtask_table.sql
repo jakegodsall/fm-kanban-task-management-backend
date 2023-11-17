2a007189069ac387de67a94af920439c1d5b874a
jakegodsall <jake.edward.godsall@gmail.com>
2023-11-17 11:43:37 +0100

    Removed NOT NULL from timestamps

--- src/main/resources/db/migration/V1__create_subtask_table.sql
+++ src/main/resources/db/migration/V1__create_subtask_table.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS sub_task;
 
 CREATE TABLE sub_task (
     id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
-    created_date TIMESTAMP NOT NULL,
-    last_modified_date TIMESTAMP NOT NULL,
+    created_date TIMESTAMP,
+    last_modified_date TIMESTAMP,
     version BIGINT NOT NULL,
     title VARCHAR(200) NOT NULL,
     is_completed BOOLEAN NOT NULL DEFAULT 0,

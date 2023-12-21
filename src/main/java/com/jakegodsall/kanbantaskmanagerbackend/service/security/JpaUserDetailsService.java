1a22a3064d74931c6a37219e5008e67b7f0c1ae9
jakegodsall <jake.edward.godsall@gmail.com>
2023-12-21 11:43:53 +0100

    added validation to convertToGrantedAuthority

--- src/main/java/com/jakegodsall/kanbantaskmanagerbackend/service/security/JpaUserDetailsService.java
+++ src/main/java/com/jakegodsall/kanbantaskmanagerbackend/service/security/JpaUserDetailsService.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -39,6 +40,10 @@ public class JpaUserDetailsService implements UserDetailsService {
     }
 
     private Set<? extends GrantedAuthority> convertToGrantedAuthorities(Set<Authority> authorities) {
+
+        if (authorities == null || authorities.isEmpty())
+            return new HashSet<>();
+
         return authorities.stream()
                 .map(Authority::getPermission)
                 .map(SimpleGrantedAuthority::new)
